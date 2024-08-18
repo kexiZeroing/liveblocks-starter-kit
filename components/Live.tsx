019b6b1b8bf6b1a733fd8f3bd0889a5a557e036f
Kexi Dang <dangkexi@gmail.com>
2024-08-18 15:08:47 +0800

    add cursor chat

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useState, useEffect, useCallback } from "react";
 import { useOthers, useMyPresence } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
-import { useCallback } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode, CursorState } from "@/types/type";
 
 function Live() {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence();
+
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -20,6 +26,9 @@ function Live() {
   }, []);
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    setCursorState({
+      mode: CursorMode.Hidden,
+    });
     updateMyPresence({
       cursor: null,
       message: null,
@@ -35,6 +44,35 @@ function Live() {
     });
   }, []);
 
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({ mode: CursorMode.Hidden });
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
+
   return (
     <div
       className="h-[100vh] w-full flex justify-center items-center text-center"
@@ -43,6 +81,15 @@ function Live() {
       onPointerDown={handlePointerDown}
     >
       <h1 className="text-3xl text-white">Hello Liveblocks</h1>
+
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        /> 
+      )}
       <LiveCursors others={others} />
     </div>
   )
